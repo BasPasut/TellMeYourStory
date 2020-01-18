5d157a1d4685e7bec17155a62126c2afc86d4faf
BasPasut <pasut.kit@gmail.com>
2020-01-18 10:42:45 +0700

    [Add] first commit

---
+++ Assets/Script/UI/MainCanvas/InteractGuideController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InteractGuideController : MonoBehaviour
+{
+    public Text interactGuideText;
+
+    public void SetInteractGuideText(string text)
+    {
+        interactGuideText.text = text;
+    }
+}
