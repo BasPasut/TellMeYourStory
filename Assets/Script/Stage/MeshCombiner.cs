0a1cce58e92aeafe29207eff390ddf820f8ce39b
BasPasut <pasut.kit@gmail.com>
2020-01-25 19:06:34 +0700

    [Add] Character Model [Mod] BasScene

---
+++ Assets/Script/Stage/MeshCombiner.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Copy meshes from children into the parent's Mesh.
+// CombineInstance stores the list of meshes.  These are combined
+// and assigned to the attached Mesh.
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+public class MeshCombiner : MonoBehaviour
+{
+    void Awake()
+    {
+        CombineMeshed();
+    }
+    
+    void CombineMeshed()
+    {
+        MeshFilter[] meshFilters = GetComponentsInChildren<MeshFilter>();
+        CombineInstance[] combine = new CombineInstance[meshFilters.Length];
+
+        int i = 0;
+        while (i < meshFilters.Length)
+        {
+            combine[i].mesh = meshFilters[i].sharedMesh;
+            combine[i].transform = meshFilters[i].transform.localToWorldMatrix;
+            meshFilters[i].gameObject.SetActive(false);
+
+            i++;
+        }
+        var meshFilter = transform.GetComponent<MeshFilter>();
+        meshFilter.mesh = new Mesh();
+        meshFilter.mesh.CombineMeshes(combine);
+        GetComponent<MeshCollider>().sharedMesh = meshFilter.mesh;
+        transform.gameObject.SetActive(true);
+
+        transform.localScale = new Vector3(1, 1, 1);
+        transform.rotation = Quaternion.identity;
+        transform.position = Vector3.zero;
+    }
+}
