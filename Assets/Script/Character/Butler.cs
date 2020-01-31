ad1730705be4cb97dc495afd18f8c948821630ad
BasPasut <pasut.kit@gmail.com>
2020-01-31 21:53:06 +0700

    [Mod] Butler animator, and fix wrong rigging model animation

--- Assets/Script/Character/Butler.cs
+++ Assets/Script/Character/Butler.cs
@@ -11,21 +11,44 @@ public class Butler : Monster
     void Start()
     {
         base.Start();
+        
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
+    {
+        WalkToPlayer();
+    }
+
+    public void WalkToPlayer()
     {
         Vector3 playerPosition = new Vector3(player.transform.position.x, 0, player.transform.position.z);
         this.transform.LookAt(playerPosition);
         if (animator.GetCurrentAnimatorStateInfo(0).IsName("walk_1"))
         {
-            if (Vector3.Distance(transform.position, playerPosition) > 2) {
+            if (Vector3.Distance(transform.position, playerPosition) > 2)
+            {
                 transform.Translate(0, 0, 0.03f);
             }
             else
             {
                 animator.SetBool("IsCloseToPlayer", true);
+            }
+        }
+    }
+
+    public void LeadToRoom()
+    {
+        animator.SetBool("IsTalkingFinish", true);
+        this.transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(transform.position, Vector3.back), 0.05f);
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("walk_2"))
+        {
+            if (Vector3.Distance(transform.position, roomPosition.position) > 2)
+            {
+                transform.Translate(0, 0, 0.03f);
+            }
+            else
+            {
+                animator.SetBool("IsCloseToRoom", true);
             }
         }
     }
