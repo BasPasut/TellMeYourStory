43e4b2bd73e2845e235f5d0b3e481d7f8950312f
BasPasut <pasut.kit@gmail.com>
2020-02-02 14:02:53 +0700

    [Mod] Butler animator and script

--- Assets/Script/Character/Butler.cs
+++ Assets/Script/Character/Butler.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -11,45 +12,64 @@ public class Butler : Monster
     void Start()
     {
         base.Start();
-        
+        StartWalking();
     }
 
     private void Update()
     {
-        WalkToPlayer();
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("walk_1"))
+        {
+            WalkToPlayer();
+            TalkingFinish();
+        }
+        else if (animator.GetCurrentAnimatorStateInfo(0).IsName("walk_2"))
+        {
+            LeadToRoom();
+        }
     }
 
     public void WalkToPlayer()
     {
         Vector3 playerPosition = new Vector3(player.transform.position.x, 0, player.transform.position.z);
         this.transform.LookAt(playerPosition);
-        if (animator.GetCurrentAnimatorStateInfo(0).IsName("walk_1"))
+        if (Vector3.Distance(transform.position, playerPosition) > 2)
         {
-            if (Vector3.Distance(transform.position, playerPosition) > 2)
-            {
-                transform.Translate(0, 0, 0.03f);
-            }
-            else
-            {
-                animator.SetBool("IsCloseToPlayer", true);
-            }
+            transform.Translate(0, 0, 0.03f);
+        }
+        else
+        {
+            animator.SetBool("IsCloseToPlayer", true);
         }
     }
 
     public void LeadToRoom()
     {
-        animator.SetBool("IsTalkingFinish", true);
-        this.transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(transform.position, Vector3.back), 0.05f);
+        var lookDirection = roomPosition.position - transform.position;
+        lookDirection.y = 0;
+        transform.rotation = Quaternion.Slerp(transform.rotation,
+        Quaternion.LookRotation(lookDirection), 0.0008f * Time.deltaTime);
+        transform.LookAt(roomPosition);
         if (animator.GetCurrentAnimatorStateInfo(0).IsName("walk_2"))
         {
-            if (Vector3.Distance(transform.position, roomPosition.position) > 2)
+            if (Vector3.Distance(transform.position, roomPosition.position) > 0.2f)
             {
                 transform.Translate(0, 0, 0.03f);
             }
             else
             {
                 animator.SetBool("IsCloseToRoom", true);
+                this.transform.LookAt(player.transform);
             }
         }
+    }
+
+    public void StartWalking()
+    {
+        animator.SetBool("StartWalking", true);
+    }
+
+    public void TalkingFinish()
+    {
+        animator.SetBool("IsTalkingFinish", true);
     }
 }
