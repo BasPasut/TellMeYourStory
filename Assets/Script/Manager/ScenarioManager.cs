fb6923972a84ee271dcbcc50954e76c75f05fc0b
Poorin <poorin.p@ku.th>
2020-03-19 22:44:35 +0700

    [Add] ScenarioManager to load scene

---
+++ Assets/Script/Manager/ScenarioManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenarioManager : MonoBehaviour
+{
+    public string sceneToLoad;
+    
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Loaded");
+            SceneManager.LoadScene(sceneToLoad, LoadSceneMode.Single);
+        }
+    }
+}
