6444a4a056e513cd41f5460127f9d6d02c4cea65
BasPasut <pasut.kit@gmail.com>
2020-03-31 15:00:10 +0700

    [Add] Boss mutant controller and animator

--- Assets/Script/Character/BossMutant.cs
+++ Assets/Script/Character/BossMutant.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class BossMutant : Monster
 {
+
+    public GameObject player;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        base.Start();   
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        RunningToPlayer();
+    }
+
+    void RunningToPlayer()
+    {
+        Vector3 playerPos = player.transform.position;
+        playerPos.y = this.transform.position.y;
+        if(this.animator.GetCurrentAnimatorStateInfo(0).IsName("roaring"))
+        {
+            this.transform.LookAt(playerPos);
+            if (this.animator.GetCurrentAnimatorStateInfo(0).IsName("run"))
+                this.transform.Translate(0, 0, 0.05f);
+        }
     }
 }
