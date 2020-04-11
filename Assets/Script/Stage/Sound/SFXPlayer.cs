ca5748f9eab4aa077371f33be882305811bef615
charin009 <charin.ta@ku.th>
2020-04-11 12:46:25 +0700

    [Mod] Animator of NPC for adding Sound [Add] SFXPlayer for play the SFX in animation clip

---
+++ Assets/Script/Stage/Sound/SFXPlayer.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXPlayer : MonoBehaviour
+{
+    private AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+       audioSource = this.transform.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayWalkButler() {
+        //audioSource.PlayOneShot(Sound.Breathing);
+    }
+
+    public void PlayWalkMutant()
+    {
+        
+    }
+
+    public void PlayWalkBossMutant()
+    {
+        
+    }
+
+    public void PlayScreamMutant()
+    {
+
+    }
+
+    public void PlayBreathingMutant()
+    {
+
+    }
+
+    public void PlayDoorOpen()
+    {
+
+    }
+
+    public void PlayDoorClose()
+    {
+
+    }
+}
