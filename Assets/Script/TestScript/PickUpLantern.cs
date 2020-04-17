085863e6b63c6155e51d77227f9fb8173435dc9e
poorinp <poorin.p@ku.th>
2020-04-17 21:51:53 +0700

    [mod] hand pose is done and playable.

--- Assets/Script/TestScript/PickUpLantern.cs
+++ Assets/Script/TestScript/PickUpLantern.cs
@@ -1,21 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
 
 [RequireComponent(typeof(Rigidbody))]
 public class PickUpLantern : MonoBehaviour
 {
-    [HideInInspector]
-    public HandForLantern activeHand = null;
+    public Hand hand;
+    private SphereCollider sphereCol;
+    public SteamVR_Action_Boolean pickUpLantern = null;
+    private SteamVR_Behaviour_Pose poseBehave = null;
 
-    void Start()
+    private void Awake()
     {
-        
+        sphereCol = this.GetComponent<SphereCollider>();
+        poseBehave = GetComponent<SteamVR_Behaviour_Pose>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 }
